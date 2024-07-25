bb5f59208f06e3a8d4b6af34fc26c98806b6a99e
russell1101 <hwesley1101@gmail.com>
2024-07-25 22:34:09 +0800

    0725

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-window.addEventListener('scroll', function() {
-    const topbar = document.getElementById('topbar');
-    if (window.scrollY > 50) {
-        topbar.classList.add('collapsed');
+window.addEventListener("scroll", function () {
+    var topbar = document.getElementById("topbar");
+    if (window.scrollY > 100) {  // 如果页面滚动超过100px
+        topbar.classList.add("collapsed");
     } else {
-        topbar.classList.remove('collapsed');
+        topbar.classList.remove("collapsed");
     }
 });
