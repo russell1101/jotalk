ea399d7ee08d91142aa557656cd8974a093631ef
russell1101 <wesley1101@gmail.com>
2024-08-16 10:39:40 +0800

    0816

--- js/script.js
+++ js/script.js
@@ -41,81 +41,36 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-// barlist 套件
-var $slider = $('.slideshow .slider'),
-    maxItems = $('.item', $slider).length,
-    dragging = false,
-    tracking,
-    rightTracking;
 
-$sliderRight = $('.slideshow').clone().addClass('slideshow-right').appendTo($('.split-slideshow'));
 
-rightItems = $('.item', $sliderRight).toArray();
-reverseItems = rightItems.reverse();
-$('.slider', $sliderRight).html('');
-for (i = 0; i < maxItems; i++) {
-    $(reverseItems[i]).appendTo($('.slider', $sliderRight));
+// 會員登入頁
+// 取得按鈕、彈出視窗和遮罩層的元素
+let loginBtn = document.getElementById("loginBtn");
+let myWindow = document.getElementById("loginWindow");
+let overlay = document.getElementById("overlay");
+let close = document.getElementById("closebtn");
+
+// 當使用者點擊登入按鈕時，顯示彈出視窗和遮罩層
+loginBtn.onclick = function () {
+    myWindow.style.display = "block";
+    overlay.style.display = "block";
 }
 
-$slider.addClass('slideshow-left');
-$('.slideshow-left').slick({
-    vertical: true,
-    verticalSwiping: true,
-    arrows: false,
-    infinite: true,
-    dots: true,
-    speed: 1000,
-    cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)'
-}).on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+// 當使用者點擊關閉按鈕時，隱藏彈出視窗和遮罩層
+close.onclick = function () {
+    myWindow.style.display = "none";
+    overlay.style.display = "none";
+}
 
-    if (currentSlide > nextSlide && nextSlide == 0 && currentSlide == maxItems - 1) {
-        $('.slideshow-right .slider').slick('slickGoTo', -1);
-        $('.slideshow-text').slick('slickGoTo', maxItems);
-    } else if (currentSlide < nextSlide && currentSlide == 0 && nextSlide == maxItems - 1) {
-        $('.slideshow-right .slider').slick('slickGoTo', maxItems);
-        $('.slideshow-text').slick('slickGoTo', -1);
-    } else {
-        $('.slideshow-right .slider').slick('slickGoTo', maxItems - 1 - nextSlide);
-        $('.slideshow-text').slick('slickGoTo', nextSlide);
-    }
-}).on("mousewheel", function (event) {
-    event.preventDefault();
-    if (event.deltaX > 0 || event.deltaY < 0) {
-        $(this).slick('slickNext');
-    } else if (event.deltaX < 0 || event.deltaY > 0) {
-        $(this).slick('slickPrev');
-    };
-}).on('mousedown touchstart', function () {
-    dragging = true;
-    tracking = $('.slick-track', $slider).css('transform');
-    tracking = parseInt(tracking.split(',')[5]);
-    rightTracking = $('.slideshow-right .slick-track').css('transform');
-    rightTracking = parseInt(rightTracking.split(',')[5]);
-}).on('mousemove touchmove', function () {
-    if (dragging) {
-        newTracking = $('.slideshow-left .slick-track').css('transform');
-        newTracking = parseInt(newTracking.split(',')[5]);
-        diffTracking = newTracking - tracking;
-        $('.slideshow-right .slick-track').css({ 'transform': 'matrix(1, 0, 0, 1, 0, ' + (rightTracking - diffTracking) + ')' });
-    }
-}).on('mouseleave touchend mouseup', function () {
-    dragging = false;
-});
+// 當使用者點擊遮罩層時，也隱藏彈出視窗和遮罩層
+overlay.onclick = function () {
+    myWindow.style.display = "none";
+    overlay.style.display = "none";
+}
 
-$('.slideshow-right .slider').slick({
-    swipe: false,
-    vertical: true,
-    arrows: false,
-    infinite: true,
-    speed: 950,
-    cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)',
-    initialSlide: maxItems - 1
-});
-$('.slideshow-text').slick({
-    swipe: false,
-    vertical: true,
-    arrows: false,
-    infinite: true,
-    speed: 900,
-    cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)'
+
+// 表單寫入localstorage
+let btn_login=document.getElementById('btn_login');
+btn_login.addEventListener('click',function(){
+    let 
 });
