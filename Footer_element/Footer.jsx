598c9e9bda2d31b39824f918bf834f8fda1d9194
russell1101 <wesley1101@gmail.com>
2024-09-10 15:10:25 +0800

    除錯

--- Footer_element/Footer.jsx
+++ Footer_element/Footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
                         <li><a href="https://policies.google.com/privacy?hl=zh-TW">隱私政策</a></li>
                         <li><a href="https://policies.google.com/terms?hl=zh-TW">服務條款</a></li>
                         <li><a href="./about.html">關於我們</a></li>
-                        <li><a href="#">店家登入</a></li>
+                        {/* <li><a href="#">店家登入</a></li> */}
                     </ul>
                     <ul id="social">
                         <li><a id="x" href="https://x.com/"></a></li>
